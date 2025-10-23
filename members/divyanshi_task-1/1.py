a914422eb4cdbda9955eef592caf6e60438bc717
div-14514 <diivyanshi.15@gmail.com>
2025-10-23 19:35:25 +0530

    Update 1.py

--- members/divyanshi_task-1/1.py
+++ members/divyanshi_task-1/1.py
@@ -3,22 +3,15 @@ positive = 0
 negative = 0
 mood_dict = {"love": 1, "happy": 1,"😍": 1,"great": 1,"good": 1,
              "hate": -1,"sad": -1,"😫": -1,"angry": -1,"bad": -1}
-
-
 statement = input("Enter a sentence with words or emojis: ")
-
 word=0
 value=0
-
-
 for word, value in mood_dict.items():
     if word in statement:
         if value == 1:
             positive += 1
         elif value == -1:
             negative += 1
-
-# Compare counts and decide the mood
 if positive > negative:
     print("Mood: Positive 😊")
 elif negative > positive:
